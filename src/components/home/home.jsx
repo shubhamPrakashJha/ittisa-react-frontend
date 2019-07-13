7c80755b9565d711141fffccc2d0845e87e1cfaa
Shubham Prakash Jha <prakash.shubhamjha@gmail.com>
2019-07-13 15:49:49 +0530

    Customize Home Page

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import rebel from "../../assets/REBELS.png";
 import "./home.css";
 
 function Home() {
     return (
         <div id="header">
+            <img src={rebel} alt="Logo" />
             <h1>
                 <strong>Best Digital Marketing Company</strong>
             </h1>
-            <p>-A Google Partner Agency-</p>
+            <h3>-A Google Partner Agency-</h3>
+            <p>
+                We are a team of highly motivated, passionate millennials for
+                whom digital marketing is a way of life. Ittisa, one of the best
+                digital marketing companies in India, believes in building
+                world-class digital experiences that easily metamorphose into
+                offline experiences and help brands tell stories, build
+                communities and transform their business. We aim for absolute
+                online success– a goal that impacts every decision we make.
+            </p>
         </div>
     );
 }
