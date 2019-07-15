87d8d44d191dabace6410e44dedefb115cd477a7
Shubham Prakash Jha <prakash.shubhamjha@gmail.com>
2019-07-15 13:53:09 +0530

    Updates

---
+++ precache-manifest.63dd5dcb4f2282d28a59f1aa97b50d3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc3b357ff20c7a11fa0d20d81fb0cf7",
+    "url": "/ittisa-react-frontend/index.html"
+  },
+  {
+    "revision": "ac045fcd4286a0f7b29d",
+    "url": "/ittisa-react-frontend/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b26d178552280f4bc86c",
+    "url": "/ittisa-react-frontend/static/css/main.e75991fc.chunk.css"
+  },
+  {
+    "revision": "ac045fcd4286a0f7b29d",
+    "url": "/ittisa-react-frontend/static/js/2.ea84d0d2.chunk.js"
+  },
+  {
+    "revision": "b26d178552280f4bc86c",
+    "url": "/ittisa-react-frontend/static/js/main.3b826a5c.chunk.js"
+  },
+  {
+    "revision": "19c8852e0efff8bb68f3",
+    "url": "/ittisa-react-frontend/static/js/runtime~main.ecfe8176.js"
+  },
+  {
+    "revision": "90f0e80b58b842a84f470ba2f7d4c13e",
+    "url": "/ittisa-react-frontend/static/media/REBELS.90f0e80b.png"
+  }
+]);
